6fd5e136bdc62625c3b8ee3045d9c075d59b19db
Gabriel Magaton <gabriel.magaton@meutudo.app>
2026-02-13 20:01:29 -0300

    config seed and backend

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Running database migrations..."
 npx prisma migrate deploy
 
+echo "Running database seed..."
+npx prisma db seed
+
 echo "Starting application..."
 exec node dist/src/main
